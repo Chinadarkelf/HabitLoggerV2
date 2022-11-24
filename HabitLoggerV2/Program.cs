7059f7d0124cc500a6cde557c7640fe189ec91f1
Jerry Jacobson <joeyj96@live.com>
2022-11-23 23:14:57 -0600

    Add project files.

---
+++ HabitLoggerV2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // path for Sqlite connection
+            var connectionString = @"Data Source=habit-Tracker.db";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = @"CREATE TABLE NOT EXISTS drinking_water
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT
+                                        Date TEXT
+                                        Quantity INTEGER";
+
+                tableCmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
